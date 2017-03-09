c4d7a935fe7949cbf46c5db15dca911b24027438
WillySung <wanny820514@gmail.com>
2017-03-09 23:26:24 +0800

    first commit:use TK to develope control ui on PC

---
+++ serverUI.py
@@ -0,0 +1,56 @@
+#importing modules required
+from ttk import *
+import Tkinter as tk
+from Tkinter import *
+import cv2
+from PIL import Image, ImageTk
+import os
+import numpy as np
+import socket
+import cv2
+
+def recvall(server, count):
+    buf = b''
+    while count:
+        newbuf = server.recv(count)
+        if not newbuf: return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
+
+def show_vid(): 
+    length = recvall(conn,16)
+    stringData = recvall(conn, int(length))
+    data = np.fromstring(stringData, dtype='uint8')
+    decimg=cv2.imdecode(data,1)
+    pic = cv2.cvtColor(decimg, cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(pic)
+    imgtk = ImageTk.PhotoImage(image=img)
+    lmain.imgtk = imgtk
+    lmain.configure(image=imgtk)
+    lmain.after(10, show_vid)	
+
+# TCP address and port
+TCP_IP = '140.116.164.19'
+TCP_PORT = 5001
+
+# AF_INET -> IPv4, SOCK_STREAM -> TCP
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+server.bind((TCP_IP, TCP_PORT))
+print 'Socket bind complete'
+server.listen(True)
+print 'Socket now listening'
+conn, addr = server.accept()
+print 'Connected with ' + addr[0] + ':' + str(addr[1])
+
+if __name__ == '__main__':
+    root=tk.Tk()                                     #assigning root variable for Tkinter as tk
+    lmain = tk.Label(master=root)
+    lmain.grid(column=0, rowspan=4, padx=5, pady=5)
+    root.title("Odroid Server")                      #you can give any title
+    show_vid()
+    root.mainloop()                                  #keeps the application in an infinite loop so it works continuosly
+
+server.close()
+cv2.destroyAllWindows() 
