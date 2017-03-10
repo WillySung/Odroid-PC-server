bb01b18156a336e83855db4fbe62d2eaf103329f
WillySung <wanny820514@gmail.com>
2017-03-10 21:11:00 +0800

    set up the UI consist with button and image

--- serverUI.py
+++ serverUI.py
@@ -1,6 +1,6 @@
 #importing modules required
 from ttk import *
-import Tkinter as tk
+from Tkinter import *
 import tkFont
 import cv2
 from PIL import Image, ImageTk
@@ -23,6 +23,7 @@ print 'Socket now listening'
 conn, addr = server.accept()
 print 'Connected with ' + addr[0] + ':' + str(addr[1])
 
+#function to receive image from odroid
 def recvall(server, count):
     buf = b''
     while count:
@@ -32,7 +33,11 @@ def recvall(server, count):
         count -= len(newbuf)
     return buf
 
+#function to show the received image
 def show_vid(): 
+    label_img = Label(frame_img)
+    label_img.grid(row=0, column=0, padx=100, pady=5)       
+	
     length = recvall(conn,16)
     stringData = recvall(conn, int(length))
     data = np.fromstring(stringData, dtype='uint8')
@@ -40,15 +45,56 @@ def show_vid():
     pic = cv2.cvtColor(decimg, cv2.COLOR_BGR2RGB)
     img = Image.fromarray(pic)
     imgtk = ImageTk.PhotoImage(image=img)
-    lmain.imgtk = imgtk
-    lmain.configure(image=imgtk)
-    lmain.after(10, show_vid)	
+    label_img.imgtk = imgtk
+    label_img.configure(image=imgtk)
+    label_img.after(10, show_vid)	
+
+#function to set up the control UI
+def ui_init():
+    helv36 = tkFont.Font(family='Helvetica', size=36, weight=tkFont.BOLD)
+    frame_btn.rowconfigure((0,4), weight=1)  # make buttons stretch when
+    frame_btn.columnconfigure((0,2), weight=1)  # when window is resized
+
+    #show local ip address
+    label_ip = Label(frame_btn, text="IP:"+TCP_IP, fg="dark green",font=helv36)
+    label_ip.grid(row=0, column=1, columnspan=2)
+
+    #a label to split the label_ip and the button below
+    label = Label(frame_btn,font=(None, 36), height=3, width=20)
+    label.grid(row=1, column=1, columnspan=2)
+
+    #forward button
+    button_fwd = Button(frame_btn,text="Forward",borderwidth=4, command=act, height = 2, width = 20,font=helv36) 
+    button_fwd.grid(row=2, column=1, columnspan=2)
+
+    #backward button
+    button_bwd = Button(frame_btn,text="Backward",borderwidth=4,command=act, height = 2, width = 20,font=helv36) 
+    button_bwd.grid(row=4, column=1, columnspan=2)
+
+    #left button
+    button_left = Button(frame_btn,text="Left",borderwidth=4,command=act, height = 2, width = 10,font=helv36)
+    button_left.grid(row=3, column=0, columnspan=2)
+
+    #right button
+    button_right = Button(frame_btn,text="Right",borderwidth=4,command=act, height = 2, width = 10,font=helv36) 
+    button_right.grid(row=3, column=2, columnspan=2)
+
+# defines an event function - for click of button
+def act(): 
+    print "I-M-pressed"
 
 if __name__ == '__main__':
-    root=tk.Tk()                                     #assigning root variable for Tkinter as tk
-    lmain = tk.Label(master=root)
-    lmain.grid(column=0, rowspan=4, padx=5, pady=5)
-    root.title("Odroid Server")                      #you can give any title
+    root = Tk(className ="Server GUI")
+    width = root.winfo_screenwidth()
+    height = root.winfo_screenheight()
+    root.geometry(str(width) + "x" + str(height))
+	
+    frame_img = Frame(root)
+    frame_btn = Frame(root)
+    frame_img.pack(side = LEFT)
+    frame_btn.pack(side = RIGHT)
+	             
+    ui_init()
     show_vid()
     root.mainloop()                                  #keeps the application in an infinite loop so it works continuosly
 
